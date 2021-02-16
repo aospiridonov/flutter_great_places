5070343c059248d91c71ca8f05edd739ebe09a83
aospiridonov <aospiridonov@gmail.com>
2021-02-16 17:55:39 +0300

    add custom image input widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_great_places/providers/great_places.dart';
 import 'package:provider/provider.dart';
 
+import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,6 +19,9 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
         ),
         home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (_) => AddPlaceScreen(),
+        },
       ),
     );
   }
--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,1 +1,60 @@
 import 'package:flutter/material.dart';
+
+import '../widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  AddPlaceScreen({Key key}) : super(key: key);
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a New Place'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(height: 10.0),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            label: Text('Add Place'),
+            icon: Icon(Icons.add),
+            onPressed: () {},
+            style: ButtonStyle(
+              elevation: MaterialStateProperty.all<double>(0.0),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).accentColor),
+              foregroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).primaryColor),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
