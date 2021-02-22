9184c6e9e745e119f92f5cd55b0be691c6cd9f46
aospiridonov <aospiridonov@gmail.com>
2021-02-22 17:09:04 +0300

    rendering a dynamic map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
 import '../helpers/location_helper.dart';
+import '../screens/maps_screen.dart';
 
 class LocationInput extends StatefulWidget {
   LocationInput({Key key}) : super(key: key);
@@ -22,6 +23,21 @@ class _LocationInputState extends State<LocationInput> {
     setState(() {
       _previewImageUrl = staticMapImageUrl;
     });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (_) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
+    // ...
   }
 
   @override
@@ -59,7 +75,7 @@ class _LocationInputState extends State<LocationInput> {
               icon: Icon(Icons.map),
               label: Text('Select on Map'),
               style: Theme.of(context).textButtonTheme.style,
-              onPressed: () {},
+              onPressed: _selectOnMap,
             ),
           ],
         ),
